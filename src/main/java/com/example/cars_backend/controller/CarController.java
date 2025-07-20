ea611b5b073ad18886abc16a08136c657017b031
temich719 <temich719@mail.ru>
2025-07-20 21:34:28 +0300

    added controllers

--- src/main/java/com/example/cars_backend/controller/CarController.java
+++ src/main/java/com/example/cars_backend/controller/CarController.java
@@ -2,8 +2,12 @@ package com.example.cars_backend.controller;
 
 import com.example.cars_backend.service.CarService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,5 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CarController {
 
     private final CarService carService;
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getCarById(@PathVariable UUID id) {
+        return ResponseEntity.ok().body(Map.of("car", carService.getCarById(id)));
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getCars(@RequestParam(defaultValue = "0", required = false) int page,
+                                     @RequestParam(defaultValue = "10", required = false) int size) {
+        return ResponseEntity.ok().body(Map.of("cars", carService.getCars(page, size)));
+    }
 
 }
--- src/main/java/com/example/cars_backend/controller/UserController.java
+++ src/main/java/com/example/cars_backend/controller/UserController.java
@@ -1,9 +1,18 @@
 package com.example.cars_backend.controller;
 
+import com.example.cars_backend.dto.RestResponse;
+import com.example.cars_backend.dto.UserDto;
+import com.example.cars_backend.exception.ConvertImageToBytesException;
 import com.example.cars_backend.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,5 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping(
+            value = "/user",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public RestResponse createUser(@RequestPart("user") UserDto userDto,
+                                   @RequestPart("avatar") MultipartFile avatarImage) throws ConvertImageToBytesException {
+        userService.createUser(userDto, avatarImage);
+        return new RestResponse("User successfully created!");
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getUserById(@PathVariable UUID id) {
+        return ResponseEntity.ok().body(Map.of("user", userService.getUserById(id)));
+    }
+
+    //todo with @AuthenticationPrinciple two rest methods from service
 
 }
