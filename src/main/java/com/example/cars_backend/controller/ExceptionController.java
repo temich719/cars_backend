d57ebb11ee0f80e114239834b1305aa53dcb8aa7
temich719 <temich719@mail.ru>
2025-07-21 02:12:17 +0300

    added exception handler

--- src/main/java/com/example/cars_backend/controller/ExceptionController.java
+++ src/main/java/com/example/cars_backend/controller/ExceptionController.java
@@ -1,7 +1,28 @@
 package com.example.cars_backend.controller;
 
+import com.example.cars_backend.exception.ConvertImageToBytesException;
+import com.example.cars_backend.exception.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConvertImageToBytesException.class)
+    public ResponseEntity<String> handleConvertImageToBytesException(ConvertImageToBytesException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleThrowable(Throwable throwable) {
+        return new ResponseEntity<>(throwable.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
